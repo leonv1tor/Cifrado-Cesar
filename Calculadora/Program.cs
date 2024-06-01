c958a7f540eb1f2ff2269151b97ede0bf7465069
TarsorCase <gerardgonzalez41@gmail.com>
2024-06-01 17:53:18 -0400

    Añadiendo funcionalidades a la Calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,8 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Cifrado;
 
-  Console.WriteLine("\n***** CIFRADO CESAR *****");
-   
+  Console.WriteLine("\n***** CALCULADOR DE CIFRADO *****\n"); 
+  Console.WriteLine("1 ---> Cifrado\n2 ---> Decifrado\n3 ---> Cifrado y Decifrado\nSeleccione una opcion (1 - 2 - 3): ");
+  int optionMr = Convert.ToInt32(Console.ReadLine());
+  switch (optionMr)
+  {
+    case 1:
+  Console.WriteLine("\n***** CIFRADO CESAR *****\n"); 
   Mathematics.Cifrado();
+    break;
+    case 2:
+  Console.WriteLine("\n\n***** DECIFRADO CESAR *****");
   Mathematics.Decifrado();
+    break;
+    case 3:
+  Console.WriteLine("\n***** CIFRADO CESAR *****\n"); 
+  Mathematics.CifradoAuto();
+  Console.WriteLine("\n\n***** DECIFRADO CESAR *****");
+  Mathematics.DecifradoAuto();
+    break;
+    default:
+    break;
+  }
 
--- Cifrado/Mathematics.cs
+++ Cifrado/Mathematics.cs
@@ -1,45 +1,153 @@
 ﻿namespace Cifrado;
 
-public static class Mathematics
-{
- public static void Cifrado() { 
-  Console.WriteLine("Ingresa la frase y el desplazamiento: \n");
+public static class Mathematics{
+  private static List<string> previousCipheredWords = new List<string>();
+ public static void CifradoAuto(){ 
+  Console.WriteLine("Ingresa la frase: \n");
   string? phrase = Console.ReadLine(); // Permitir null
-    if (string.IsNullOrEmpty(phrase))
-    {
+  if (string.IsNullOrEmpty(phrase)){
+    Console.WriteLine("La entrada no puede estar vacía.");
+    return;
+  }
+  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+  List<string> cypherwords = new List<string>();
+  List<string> words = new List<string>();
+  List<char> abecedario = new List<char>{
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+  foreach (string word in wordarray){
+    words.Add(word);
+  }
+  Console.WriteLine("\nIngresa el desplazamiento: \n");
+  if (!int.TryParse(Console.ReadLine(), out int displacement)) {
+    Console.WriteLine("Invalid displacement value.");
+    return;
+  }
+  foreach (var word in words){
+    string encryptedword = "";
+    foreach (var wchar in word){
+      if (wchar >= 'a' && wchar <= 'z'){
+        int index = (abecedario.IndexOf(wchar) + displacement) % 26;
+        char yes = abecedario[index];
+        encryptedword += yes;
+      }else{
+        encryptedword += wchar; // Si no es una letra del abecedario, mantenerla sin cambios
+      }
+    }
+    cypherwords.Add(encryptedword);
+  }
+  Console.WriteLine("\nFrase cifrada: \n");
+  foreach (var cypherword in cypherwords){
+  Console.Write(cypherword + " ");
+  }
+  previousCipheredWords.AddRange(cypherwords);
+ }
+
+  public static void DecifradoAuto(){
+    List<string> words = new List<string>();
+    List<char> abecedario = new List<char>{
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    Console.WriteLine("\nIngresa el desplazamiento: \n");
+    if (!int.TryParse(Console.ReadLine(), out int displacement)) {
+      Console.WriteLine("Invalid displacement value.");
+      return;
+    }
+    foreach (var cypherword in previousCipheredWords){
+      string word = "";
+      foreach (var wchar in cypherword){
+        if (wchar >= 'a' && wchar <= 'z'){
+          int index = (abecedario.IndexOf(wchar) - displacement) % 26;
+          if(index < 0){
+            index += 26;
+          }
+          char yes = abecedario[index];
+          word += yes;
+        }else{
+          word += wchar; // Si no es una letra del abecedario, mantenerla sin cambios
+        }
+      }
+      words.Add(word);
+    }
+    Console.WriteLine("\nFrase decifrada: \n");
+    foreach (var word in words){
+    Console.Write(word + " ");
+    }
+  }
+ public static void Cifrado(){ 
+  Console.WriteLine("Ingresa la frase: \n");
+  string? phrase = Console.ReadLine(); // Permitir null
+  if (string.IsNullOrEmpty(phrase)){
+    Console.WriteLine("La entrada no puede estar vacía.");
+    return;
+  }
+  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+  List<string> cypherwords = new List<string>();
+  List<string> words = new List<string>();
+  List<char> abecedario = new List<char>{
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+  foreach (string word in wordarray){
+    words.Add(word);
+  }
+  Console.WriteLine("\nIngresa el desplazamiento: \n");
+  if (!int.TryParse(Console.ReadLine(), out int displacement)) {
+    Console.WriteLine("Invalid displacement value.");
+    return;
+  }
+  foreach (var word in words){
+    string encryptedword = "";
+    foreach (var wchar in word){
+      if (wchar >= 'a' && wchar <= 'z'){
+        int index = (abecedario.IndexOf(wchar) + displacement) % 26;
+        char yes = abecedario[index];
+        encryptedword += yes;
+      }else{
+        encryptedword += wchar; // Si no es una letra del abecedario, mantenerla sin cambios
+      }
+    }
+    cypherwords.Add(encryptedword);
+  }
+  Console.WriteLine("\nFrase cifrada: \n");
+  foreach (var cypherword in cypherwords){
+  Console.Write(cypherword + " ");
+  }
+ }
+  public static void Decifrado(){
+    Console.WriteLine("Ingresa la frase cifrada: \n");
+    string? phrase = Console.ReadLine(); // Permitir null
+    if (string.IsNullOrEmpty(phrase)){
       Console.WriteLine("La entrada no puede estar vacía.");
       return;
     }
-  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
-  List<string> words = new List<string>();
-  foreach (string word in wordarray)
-  {
-    words.Add(word);
-  }
-  Console.WriteLine("\nThe words are:");
-  foreach (var word in words)
-  {
-    Console.WriteLine(word);
-  }
- }
- public static void Decifrado() { 
-  Console.WriteLine("Ingresa la frase y el desplazamiento: \n");
-  string? phrase = Console.ReadLine(); // Permitir null
-    if (string.IsNullOrEmpty(phrase))
-    {
-      Console.WriteLine("La entrada no puede estar vacía.");
+    string[] cypherwordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+    List<string> cypherwords = new List<string>();
+    List<string> words = new List<string>();
+    List<char> abecedario = new List<char>{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    foreach (string cypherword in cypherwordarray){
+      cypherwords.Add(cypherword);
+    }
+    Console.WriteLine("\nIngresa el desplazamiento: \n");
+    if (!int.TryParse(Console.ReadLine(), out int displacement)) {
+      Console.WriteLine("Invalid displacement value.");
       return;
     }
-  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
-  List<string> words = new List<string>();
-  foreach (string word in wordarray)
-  {
-    words.Add(word);
+    foreach (var cypherword in cypherwords){
+      string word = "";
+      foreach (var wchar in cypherword){
+        if (wchar >= 'a' && wchar <= 'z'){
+          int index = (abecedario.IndexOf(wchar) - displacement) % 26;
+          if(index < 0){
+            index += 26;
+          }
+          char yes = abecedario[index];
+          word += yes;
+        }else{
+          word += wchar; // Si no es una letra del abecedario, mantenerla sin cambios
+        }
+      }
+      words.Add(word);
+    }
+    Console.WriteLine("\nFrase decifrada: \n");
+    foreach (var word in words){
+    Console.Write(word + " ");
+    }
   }
-  Console.WriteLine("\nThe words are:");
-  foreach (var word in words)
-  {
-    Console.WriteLine(word);
-  }
- }
 }
