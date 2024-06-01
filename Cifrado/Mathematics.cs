a3ee0907fd6fdbaec93346c1f09c6fed337889de
TarsorCase <gerardgonzalez41@gmail.com>
2024-06-01 10:05:07 -0400

    Añadiendo funcionalidades a la Calculadora

--- Cifrado/Mathematics.cs
+++ Cifrado/Mathematics.cs
@@ -2,25 +2,44 @@
 
 public static class Mathematics
 {
- public static double Esfera(double num1) { 
-  
- double ans = (4.0/3.0) * Math.PI * Math.Pow(num1, 3);
- 
- return ans;
+ public static void Cifrado() { 
+  Console.WriteLine("Ingresa la frase y el desplazamiento: \n");
+  string? phrase = Console.ReadLine(); // Permitir null
+    if (string.IsNullOrEmpty(phrase))
+    {
+      Console.WriteLine("La entrada no puede estar vacía.");
+      return;
+    }
+  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+  List<string> words = new List<string>();
+  foreach (string word in wordarray)
+  {
+    words.Add(word);
+  }
+  Console.WriteLine("\nThe words are:");
+  foreach (var word in words)
+  {
+    Console.WriteLine(word);
+  }
  }
- public static double Lote(double num1, double num2) {  
-
- double ans2 = num1 * num2;
- 
- return ans2;
- }
- public static double Edificio(double num1, double num2) {  
- 
-  double alphagrados = num2 * (Math.PI/180.0);
-  double tanalpha =Math.Tan(alphagrados); 
-
- double ans3 = num1 * tanalpha;
-
- return ans3;
+ public static void Decifrado() { 
+  Console.WriteLine("Ingresa la frase y el desplazamiento: \n");
+  string? phrase = Console.ReadLine(); // Permitir null
+    if (string.IsNullOrEmpty(phrase))
+    {
+      Console.WriteLine("La entrada no puede estar vacía.");
+      return;
+    }
+  string[] wordarray = phrase.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+  List<string> words = new List<string>();
+  foreach (string word in wordarray)
+  {
+    words.Add(word);
+  }
+  Console.WriteLine("\nThe words are:");
+  foreach (var word in words)
+  {
+    Console.WriteLine(word);
+  }
  }
 }
